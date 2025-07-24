704b4df6e46db4818ad8c8751825a02579f6d31b
AJ Carpinello <ajcarp81003@gmail.com>
2025-07-24 14:06:27 -0400

    postgreSQL switch

--- src/main/java/javaFiles/JdbcUtil.java
+++ src/main/java/javaFiles/JdbcUtil.java
@@ -10,19 +10,25 @@ public class JdbcUtil {
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Class.forName("com.mysql.cj.jdbc.Driver");
+        	Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://mysql:3306/JDBC_Learning";
-        String username = System.getenv("DB_USER");
-        String password = System.getenv("DB_PASS");
+        // String url = "jdbc:mysql://mysql:3306/JDBC_Learning";
+    	String host = System.getenv("DB_HOST"); 
+        String port = System.getenv("DB_PORT"); 
+        String db   = System.getenv("DB_NAME");
+        String user = System.getenv("DB_USER");
+        String pass = System.getenv("DB_PASS");
+
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + db;
 
 
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(url, user, pass);
     }
 
     public static void closeResources(Connection connect, Statement stmt) throws SQLException {
